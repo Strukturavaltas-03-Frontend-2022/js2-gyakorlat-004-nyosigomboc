3ccd6ee032b6a43754ee2cc830b658fc174374c5
Gyozo Miklos <nyosdroid@gmail.com>
2022-08-15 13:18:41 +0200

    done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,20 @@
 
+const isWorkDay = d => 1 <= d.getDay() && d.getDay() <=5
 
+const countOfWorkingDaysBetween = (start, end) =>
+{
+    let count = 0
+    for(let d=start; d<=end; d.setDate(d.getDate() + 1))
+        count += isWorkDay(d) ? 1 : 0
+    return count
+}
 
+const countOfWorkingDays = () =>
+{
+    const thisYear = (new Date()).getFullYear()
+    const startDate = new Date(thisYear, 0, 1)
+    const endDate =new Date(thisYear, 8-1, 31)
+    return countOfWorkingDaysBetween(startDate, endDate)
+}
 
-
-
-// export default countOfWorkingDays;
+export default countOfWorkingDays;
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,7 +1,8 @@
 
+const customDateFormats = d => (
+{
+    short: d.toLocaleString('hu', {dateStyle: 'medium', timeStyle: 'short'}),
+    long: d.toLocaleString('hu', {dateStyle: 'long', timeStyle: 'medium'})
+})
 
-
-
-
-
-// export default customDateFormats;
+export default customDateFormats;
